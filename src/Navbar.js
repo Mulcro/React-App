f4af6a2090fa69a1655ddfbef6cd18c80a8a2de9
Mulero Alamou <Mulero.alamou@gmail.com>
2022-09-07 09:08:34 +0100

    created navbar

---
+++ src/Navbar.js
@@ -0,0 +1,15 @@
+const Navbar = () => {
+    return ( 
+        <nav className="navbar">
+            <div className="title">
+                <h1>LeroBlog</h1>
+            </div>
+            <div className="links">
+                <a href="/">Home</a>
+                <a href="/">New Blog</a>
+            </div>
+        </nav>
+     );
+}
+ 
+export default Navbar;
